a23cdacd2da07f4d26bd62bd01a90b7eb9064d5c
Sarbik Betal <sarbikb55@gmail.com>
2019-03-12 17:50:01 +0530

    Added particle js

---
+++ script.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", particlesJS.load('particle-box', 'assets/particles.json', function () {
+    console.log('callback - particles.js config loaded');
+}));
