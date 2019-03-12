abcd03c9e2866ab1334b48837062ec32ecbbb2d4
Sarbik Betal <sarbikb55@gmail.com>
2019-03-13 03:44:13 +0530

    Slider implemented

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
 document.addEventListener("DOMContentLoaded", particlesJS.load('particle-box', 'assets/particles.json', function () {
     console.log('callback - particles.js config loaded');
 }));
+
+
+$(function () {
+    var Page = (function () {
+        var $navArrows = $('#nav-arrows').hide(),
+            $navOptions = $('#nav-options').hide(),
+            $shadow = $('#shadow').hide(),
+            slicebox = $('#sb-slider').slicebox({
+                onReady: function () {
+
+                    $navArrows.show();
+                    $navOptions.show();
+                    $shadow.show();
+
+                },
+                orientation: 'h',
+                cuboidsCount: 3,
+                autoplay: true,
+                interval: 5000
+            }),
+
+            init = function () {
+
+                initEvents();
+
+            },
+            initEvents = function () {
+
+            };
+
+        return {
+            init: init
+        };
+
+    })();
+
+    Page.init();
+});
